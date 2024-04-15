0aca0a9eb13cb84f648685d7e4b03924a2b4ec1e
NotaWhaIe <NotaWhaIe@yandex.kz>
2024-04-15 16:09:29 +0500

    backUp test show only louded links in project

--- Strana.Revit/RevitCommands/Test.cs
+++ Strana.Revit/RevitCommands/Test.cs
@@ -23,97 +23,32 @@ namespace Strana.Revit.HoleTask.RevitCommands
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
-            var nestedFamilyNames = new List<string>
-            {
-                "(Отв_Задание)_Стены_Прямоугольное",
-                "(Отв_Задание)_Перекрытия_Прямоугольное"
-            };
 
-            using (Transaction trans = new Transaction(doc, "Копирование вложенных семейств с параметрами"))
+            var loadedLinks = GetLinkedFiles(doc)
+                .Where(link => RevitLinkType.IsLoaded(doc, link.Id))
+                .Select(link => link.Name)
+                .ToList();
+
+            var debag=loadedLinks.Count;
+
+            using (Transaction trans = new Transaction(doc, "test"))
             {
                 TransactionHandler.SetWarningResolver(trans);
                 trans.Start();
-
-                var levels = new FilteredElementCollector(doc)
-                    .OfClass(typeof(Level))
-                    .Cast<Level>()
-                    .OrderBy(l => l.Elevation)
-                    .ToList();
-
-                var allInstances = new FilteredElementCollector(doc)
-                    .OfClass(typeof(FamilyInstance))
-                    .WhereElementIsNotElementType()
-                    .Cast<FamilyInstance>()
-                    .ToList();
-
-                foreach (var instance in allInstances)
-                {
-                    var dependentIds = instance.GetSubComponentIds();
-                    foreach (ElementId id in dependentIds)
-                    {
-                        FamilyInstance nestedInstance = doc.GetElement(id) as FamilyInstance;
-                        if (nestedInstance != null && nestedFamilyNames.Contains(nestedInstance.Symbol.FamilyName))
-                        {
-                            LocationPoint locationPoint = nestedInstance.Location as LocationPoint;
-                            if (locationPoint != null)
-                            {
-                                Level chosenLevel = ChooseLevel(levels, locationPoint.Point.Z);
-
-                                if (chosenLevel != null)
-                                {
-                                    double offset = locationPoint.Point.Z - chosenLevel.Elevation;
-                                    FamilyInstance newInstance = CreateFamilyInstanceWithLevel(doc, nestedInstance, locationPoint.Point, chosenLevel, offset);
-
-                                    // Копирование значений параметров
-                                    CopyParameters(nestedInstance, newInstance);
-                                }
-                            }
-                        }
-                    }
-                }
-
                 trans.Commit();
             }
 
             return Result.Succeeded;
         }
 
-        private Level ChooseLevel(List<Level> levels, double zPoint)
+        private static List<RevitLinkType> GetLinkedFiles(Document doc)
         {
-            Level closestLevelBelow = levels.LastOrDefault(l => l.Elevation < zPoint);
-            Level closestLevelAbove = levels.FirstOrDefault(l => l.Elevation > zPoint);
-            return closestLevelBelow ?? closestLevelAbove;
-        }
+            FilteredElementCollector collector = new(doc);
+            var linkedElements = collector
+                .OfClass(typeof(RevitLinkType))
+                .OfType<RevitLinkType>();
 
-        private FamilyInstance CreateFamilyInstanceWithLevel(Document doc, FamilyInstance originalInstance, XYZ point, Level level, double offset)
-        {
-            FamilySymbol symbol = originalInstance.Symbol;
-            FamilyInstance newInstance = doc.Create.NewFamilyInstance(point, symbol, level, StructuralType.NonStructural);
-            Parameter elevationParam = newInstance.get_Parameter(BuiltInParameter.INSTANCE_ELEVATION_PARAM);
-            if (elevationParam != null && elevationParam.IsReadOnly == false)
-            {
-                elevationParam.Set(offset);
-            }
-            return newInstance;
-        }
-
-        private void CopyParameters(FamilyInstance originalInstance, FamilyInstance newInstance)
-        {
-            var parameterNames = new List<string> { "Глубина", "Ширина", "Высота" };
-
-            foreach (string paramName in parameterNames)
-            {
-                Parameter originalParam = originalInstance.LookupParameter(paramName);
-                Parameter newParam = newInstance.LookupParameter(paramName);
-
-                // Проверяем, существуют ли параметры и не является ли параметр нового экземпляра только для чтения
-                if (originalParam != null && newParam != null && !newParam.IsReadOnly)
-                {
-                    // Копируем значение из оригинального экземпляра в новый
-                    double value = originalParam.AsDouble();
-                    newParam.Set(value);
-                }
-            }
+            return linkedElements.ToList();
         }
     }
 }
